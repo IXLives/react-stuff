9b38a7a2127625d37aeba4f56dfb7ce60cb3f932
IXLives <bryant.ian87@gmail.com>
2025-08-27 16:51:38 -0400

    chore: scaffold youtube clone project

---
+++ projects/react-youtube/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className='min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue via-gray-600 to-gray-900 text-white px-4'>
+        <h1 className='text-5xl font-extrabold mb-8 text-white drop-shadow-lg uppercase tracking-wide'>YouTube</h1>
+    </div>
+  )
+}
+
+export default App
